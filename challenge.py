087f5a0944e3be4a721765cf00e451ea2c0e0d87
Shambhavi Singh <shambhavisingh@Shambhavis-MacBook-Pro.local>
2022-01-31 14:10:08 -0700

    Second commit

--- challenge.py
+++ challenge.py
@@ -1,1 +1,10 @@
 #read a csv file
+import csv
+file_name = input("Enter CSV file name:\n")
+inp = open(file_name, "r")
+csvreader = csv.reader(inp)
+header = []
+header = next(csvreader)
+print(header)
+
+
